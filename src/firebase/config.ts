68bb0a6a5b4851eb8ddf51b9cdd8dc92e8d53985
Louis H <louishatte@gmail.com>
2021-12-09 14:29:51 +0100

    Add firebase and ESLint

---
+++ src/firebase/config.ts
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+interface FirebaseConfig {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+}
+
+const firebaseConfig: FirebaseConfig = {
+    apiKey: "AIzaSyAbrFzaoRStG-B4Qs94JKo4ajDK8F820TQ",
+    authDomain: "mini-asana.firebaseapp.com",
+    projectId: "mini-asana",
+    storageBucket: "mini-asana.appspot.com",
+    messagingSenderId: "470298841632",
+    appId: "1:470298841632:web:fcad215e0cd7b960966245"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const fStore = firebase.firestore();
+const fAuth = firebase.auth();
+
+const fTimestamp = firebase.firestore.Timestamp;
+
+export { fStore, fAuth, fTimestamp };
